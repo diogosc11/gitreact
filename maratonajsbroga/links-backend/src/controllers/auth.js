212dba5c55d74c92f1da840821a2e6d38b8925e9
Diogo Scheffer <diogoscheffer9@hotmail.com>
2021-01-23 15:25:16 -0300

    aaa

--- maratonajsbroga/links-backend/src/controllers/auth.js
+++ maratonajsbroga/links-backend/src/controllers/auth.js
@@ -1,19 +1,30 @@
 const express = require('express');
-const {account} = require('../models')
+const { account } = require('../models')
 const bcrypt = require('bcrypt');
-const {accountSignUp} = require('../validators/account');
+const { accountSignUp, accountSignIn } = require('../validators/account');
 const { getMessage } = require('../helper/validator');
+const { generateJwt, generateRefreshJwt } = require('../helper/jwt')
 
 const router = express.Router();
 
 const saltRounds = 10;
 
-router.get('/sign-in', (req, res) => {
-    return res.json('Sign in');
+router.post('/sign-in', accountSignIn, async(req, res) => {
+    const { email, password } = req.body;
+
+    const findAccount = await account.findOne({where: {email}});
+
+    const match = findAccount ? bcrypt.compareSync( password, findAccount.password ) : null;
+    if(!match) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
+
+    const token = generateJwt({id: findAccount.id});
+    const refreshToken = generateRefreshJwt({id: findAccount.id});
+
+    return res.jsonOK( findAccount, getMessage('account.signup.sucess'), { token, refreshToken });
 })
 
-router.get('/sign-up', accountSignUp ,async(req, res) => {
-    const {email, password} = req.body;
+router.post('/sign-up', accountSignUp, async(req, res) => {
+    const { email, password } = req.body;
 
     const findAccount = await account.findOne({where: {email}});
     if(findAccount) return res.jsonBadRequest(null, getMessage('account.signup.email_exists'));
@@ -21,7 +32,10 @@ router.get('/sign-up', accountSignUp ,async(req, res) => {
     const hash = bcrypt.hashSync(password, saltRounds);
     const newAccount = await account.create({email, password: hash});
 
-    return res.jsonOK(newAccount, getMessage('account.signup.sucess'));
+    const token = generateJwt({id: newAccount.id});
+    const refreshToken = generateRefreshJwt({id: newAccount.id});
+
+    return res.jsonOK( newAccount, getMessage('account.signup.sucess'), {token, refreshToken} );
 })
 
 module.exports = router;
